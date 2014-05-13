654f3f00f3ab83bf8923ab83f121e95f514fe572
JoY <JoY@JoY-PC>
2014-05-13 16:32:22 +0700

    Lab

---
+++ src/com/rmuti/datastructure/ArrayListStack.java
@@ -0,0 +1,25 @@
+package com.rmuti.datastructure;
+
+public class ArrayListStack {
+
+	public static void main(String[] args) {
+		
+	}
+		ArrayList list = new ArrayList();
+		public void push(Object e) {
+			list.add(e);
+		}
+		public Object pop() {
+			Object a = list.get(list.size());
+			list.remove(list.size());
+			return a;
+		}
+		public Object peek() {
+			return list.get(list.size());
+		}
+
+		@Override
+		public String toString() {		
+			return String.valueOf(list);
+		}
+	}
